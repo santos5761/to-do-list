2ff7206f3f8bd37d49bbd7ee0ed84198f569cdda
Gabriel Santos Gonçalves <gabriel.sgo@hotmail.com>
2025-03-24 11:09:34 -0300

    Adicionando a função que salva uma nova task, e comentando o código

--- script.js
+++ script.js
@@ -1,39 +1,44 @@
 // TO-DO-LIST
 
 // Capturando elementos
-
 var text = document.getElementsByTagName('input')[0];
 var btnSave = document.getElementById('btnSalvar');
 var container = document.querySelector('.container');
 
 // Criando um array no localstorage
 var array = [];
-localStorage.setItem('Tarefas', JSON.stringify(array))
+// Traz a key Tarefas do localStorage
+var chaves = localStorage.key('Tarefas');
+// Verifica se essa chave existe
+if(chaves == null){
+    // Caso não existam, estabelece a chave com o valor de um array vazio
+    localStorage.setItem('Tarefas', JSON.stringify(array))
+}
 // Função que gera tarefa
 
 function newTask(txt){
     // Criando e anexando cada elemento da task
     var tarefa = document.createElement('div');
     tarefa.classList.add('tarefa');
-
+    // txt da task
     var name = document.createElement('p');
     name.innerText = txt;
     tarefa.appendChild(name);
-    
+    // btn de check da task
     var btnCheck = document.createElement('button');
     btnCheck.id = 'checkTask';
     tarefa.appendChild(btnCheck);
     var img0 = document.createElement('img');
     img0.src = 'http://127.0.0.1:5500/assets/png.webp'
     btnCheck.appendChild(img0);
-
+    // btn delete da task
     var btnDelete = document.createElement('button');
     btnDelete.id = 'deleteTask';
     tarefa.appendChild(btnDelete);
     var img1 = document.createElement('img');
     img1.src = 'assets/trash.png';
     btnDelete.appendChild(img1);
-
+    // anexando task ao container
     container.appendChild(tarefa)
 
     // Fazendo o check da tarefa
@@ -42,11 +47,6 @@ function newTask(txt){
         check(img0)
     })
 
-    // Armazenando no localStorage
-    var recoveryArray = JSON.parse(localStorage.getItem('Tarefas'))
-    recoveryArray.push(txt)
-    localStorage.setItem('Tarefas', JSON.stringify(recoveryArray))
-
 }
 
 // Função que alterna a confirmação da tarefa
@@ -54,26 +54,37 @@ function check(img){
     if(img.src == 'http://127.0.0.1:5500/assets/check.png'){
         img.src = 'assets/png.webp';
     }
-    else if(img.src == 'http://127.0.0.1:5500/assets/png.webp'){
+    else{
         img.src = 'assets/check.png'
     }
-    
 }
-
-// Função que deleta Task
-
-newTask('NomeTarefa')
-
 
 // Função que traz todas as tasks armazenadas
 function trazendoTasks(){
+    // Limpando Container
+    container.innerHTML = '';
+
+    // Trazendo conteudo do localStorage
     var recoveryTasks = JSON.parse(localStorage.getItem('Tarefas'));
+    // Percorrendo cada elemento do localStorage e aplicando a função que gera car da tarefa
     recoveryTasks.forEach(nameTask => {
         newTask(nameTask)
     });
 }
 
 // Trazendo tasks que estão no localStorage ao entrar no site
-trazendoTasks()
+ trazendoTasks()
 
-// Adicionando nova Task
+// Adicionando nova Task ao clicar no btnSave
+btnSave.addEventListener('click', function(){
+    // Trazendo todos os elementos do localStorage
+    var recoveryArray = JSON.parse(localStorage.getItem('Tarefas'))
+    // Adicionando o novo valor ao array
+    recoveryArray.push(text.value)
+    // Devolvend o array para o localStorage
+    localStorage.setItem('Tarefas', JSON.stringify(recoveryArray));
+
+    // Executando a função que traz as tasks
+    trazendoTasks()
+})
+
